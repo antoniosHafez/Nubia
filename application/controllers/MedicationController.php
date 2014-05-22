e8cfbda5f49011b54d384cdbd6b764760cd6096a
antonios <antonios@antonios-Inspiron-1564.(none)>
2014-05-22 19:42:34 +0200

    First Commit

---
+++ application/controllers/MedicationController.php
@@ -0,0 +1,25 @@
+<?php
+
+class MedicationController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function addAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
