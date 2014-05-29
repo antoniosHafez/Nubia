76d8f085518d89fdb32d4fa04739660045eeb6ed
intake34 <ashrafkottb@rocketmail.com>
2014-05-30 01:38:17 +0200

    live visit

--- application/controllers/PhysicianvisitController.php
+++ application/controllers/PhysicianvisitController.php
@@ -65,8 +65,82 @@ class PhysicianvisitController extends Zend_Controller_Action
     }
     }
 
+    public function liveAction()
+    {
+           $patientModel = new Application_Model_Patient();
+        $personModel = new Application_Model_Person();
+        $addressModel = new Application_Model_Address();
+        $visitModel = new Application_Model_Visit();
+        ///
+        $action = array("action" => "doctor");
+        $VisitForm = new Application_Form_Visit($action);
+
+        $id = $this->_request->getParam("vid");
+        $visit_model = new Application_Model_Visit();
+        $data = $visit_model->selectVisitById($id);
+
+        $VisitForm->populate($data);
+        
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->_request->getPost();
+            if ($VisitForm->isValid($formData))
+            {
+                
+                
+                 $visit_model = new Application_Model_Visit();
+                 unset($formData['submit']);
+                 $visit_model->editVisit($formData,$id);
+                       $this->redirect("visit/view/id/".$id);     
+                      
+                      
+            }
+        }
+        $this->view->visitform = $VisitForm;
+        ///
+        if($this->getRequest()-> isGet()){
+            if($this->hasParam("patientId")){
+                $patientId = $this->getParam("patientId");
+                $this->view->patientId = $patientId;
+                $fullData = $patientModel->getPatientFullDataById($patientId);
+                $this->view->fullData = $fullData;
+            }
+            if($this->hasParam("showPatientHistory")){
+                $patientId = $this->getParam("showPatientHistory");
+                $medicationModel = new Application_Model_MedicationHistory();
+                $this->view->medicationData = $medicationModel -> getMedicationByPatientID($patientId);
+                $diseaseModel = new Application_Model_DiseaseHistory();
+                $this->view->diseaseData = $diseaseModel ->getDiseaseHistoryByPatientID($patientId); 
+                $surgeryModel = new Application_Model_SugeryHistory();
+                $this->view->surgeryData = $surgeryModel->getSugeryHistoryByPatientID($patientId);
+                $this->view->patientId = $patientId;
+                $this->render("list-patient-medical-history");
+            }
+            if($this->hasParam("previousVisits")){
+                $patientId = $this->getParam("previousVisits");
+                $this->view->patientId = $patientId;
+                $this->view->previousVisits = $visitModel->getPreviousVisits($patientId);
+                $this->render("list-previous-visits");
+            }
+            if($this->hasParam("pendingVisits")){
+                $patientId = $this->getParam("pendingVisits");
+                $this->view->patientId = $patientId;
+                $this->view->pendingVisits = $visitModel->getPendingVisits($patientId);
+                $this->render("list-pending-visits");
+            }
+            if($this->hasParam("acceptedVisits")){
+                $patientId = $this->getParam("acceptedVisits");
+                $this->view->patientId = $patientId;
+                $this->view->acceptedVisits = $visitModel->getAcceptedVisits($patientId);
+                $this->render("list-accepted-visits");
+            }            
+        }
+    
+    }
+
 
 }
+
+
 
 
 
--- application/forms/Visit.php
+++ application/forms/Visit.php
@@ -19,7 +19,8 @@ public function __construct($param,$options = null) {
                  
              $this->addElement('text', 'date', array('label' => 'visit request date :', 'required' => true));
         $this->addElement('textarea', 'description', array('label' => 'description :', 'required' => true, 'filters' => array('StringTrim')));
-         
+           if($this->action == "edit" || $this->action == "add"){
+             
           /////drop down list and filled with physicians
         
         $physican = new Zend_Form_Element_Select('physican_id', array('multiple' => false));
@@ -29,6 +30,7 @@ public function __construct($param,$options = null) {
             $physican->addMultiOption($phy['id'], $phy['name']);
         }
         $physican->setLabel("Choose physician");
+        $physican->setOrder(5);
         $this->addElement($physican);
         ///////
 
@@ -41,6 +43,7 @@ public function __construct($param,$options = null) {
             $patient->addMultiOption($pat['id'], $pat['name']);
         }
         $patient->setLabel("Choose patient");
+        $physican->setOrder(6);
         $this->addElement($patient);
         ///////
         
@@ -53,27 +56,30 @@ public function __construct($param,$options = null) {
             $group->addMultiOption($grp['id'], $grp['name']);
         }
         $group->setLabel("Choose Group");
+        $physican->setOrder(7);
         $this->addElement($group);
         ///////
 
            
           $this->addElement('radio', "type", array(
             'label' => 'Type',
-              
+             'order'=>2, 
             'multiOptions' => array(
                 'kashf' => 'kashf',
                 'Estshara' => 'Estshara',
             ),
               'Value' => 'kashf'
         ));
-            $this->addElement('textarea', 'notes', array('label' => 'notes :', 'required' => true, 'filters' => array('StringTrim')));
               
               $checkbox = new Zend_Form_Element_Checkbox('depandency');
                 $checkbox->setLabel('Depandency');
                 $checkbox->setUncheckedValue("A");
+                $physican->setOrder(8);
                 $this->addElement($checkbox);
-              
-        $this->addElement('submit', 'submit', array('ignore'=> true,'label'=> 'submit'));
+           }
+            $this->addElement('textarea', 'notes', array('label' => 'notes :', 'required' => true, 'filters' => array('StringTrim')));
+           
+        $this->addElement('submit', 'submit', array('ignore'=> true,'label'=> 'submit','order'=>10));
     
 
     }
